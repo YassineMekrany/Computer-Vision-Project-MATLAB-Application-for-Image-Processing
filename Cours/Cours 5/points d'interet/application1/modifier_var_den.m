0931e4e85f562dc275f77e88e9ceeb2bc3ccf1ba
YassineMekrany <yassine2001mk@gmail.com>
2024-08-03 07:09:39 +0100

    Remove Cours/ and TPs/ directories

--- Cours/Cours 5/points d'interet/application1/modifier_var_den.m
+++
@@ -1,5 +0,0 @@
-function modifier_var_den
-global baravancD  edittext3
-    d=get(baravancD,'Value');
-    den=num2str(d);
-    set(edittext3,'String',den);
--- TPs/TP-imagerie-version-finale-2021/TP_Imagerie_matlab/application1_points_interet/modifier_moyenne.m
+++
@@ -1,9 +0,0 @@
-function modifier_moyenne
-global baravancM edittext1
-    m=get(baravancM,'Value');
-    moy=num2str(m);
-    set(edittext1,'String',moy);
-    
-    
-    
-    
